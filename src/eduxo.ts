31eecb652cc70cf6b325bfa6d5650e4bfc87e623
han4wluc <han4wluc@gmail.com>
2018-06-30 12:58:54 +0800

    add types

--- src/eduxo.ts
+++ src/eduxo.ts
@@ -1,61 +1,116 @@
-import {
-  AnyAction,
-  combineReducers,
-  createStore,
-  Dispatch,
-  Middleware,
-  Store,
-} from 'redux';
+import { combineReducers, createStore, Store, applyMiddleware } from 'redux';
+import { connect } from 'react-redux';
 
-export interface IAction { type: string; payload?: object | string }
+// declare module 'redux-logic'
+// import { createLogicMiddleware, createLogic } from 'redux-logic';
+let reduxLogic = require('redux-logic') 
+const { createLogicMiddleware, createLogic } = reduxLogic;
+
+export interface IAction {
+  type: string;
+  payload?: object | string;
+}
 
 export type reducerType<T> = (state: T, action: IAction) => T;
-export interface IReducers<T> { [s: string]: reducerType<T> }
+export interface IReducers<T> {
+  [s: string]: reducerType<T>;
+}
 
 export interface IModel<T, D> {
   namespace: string;
   initialState: T;
-
   reducers: IReducers<T>;
-  generateDispatchers: (dispatch: (action: IAction) => void) => D;
+  generateDispatchers: (dispatch: (action: IAction) => void, deps?: object) => D;
+  logics?: Array<any>;
 }
 
-export default class Eduxora<D> {
-  public store: Store;
-  public models: Array<IModel<any, any>>;
-  public dispatchers: D;
+interface A {
+  [key: string]: any;
+}
 
-  constructor(initialDispatchers: D) {
-    this.store = {
-      dispatch: action => {
-        return action;
-      },
-      getState: () => {return},
-      subscribe: () => () => {return},
-      replaceReducer: () => {return},
-    };
+// export interface ISomeObject {
+//   home: any,
+//   [index: number]: any;
+//   [index: string]: any;
+//   // [key: number]: any;
+//   // [key: string]: any;
+//   // [key: any]: any;
+// }
+
+// const asdf : ISomeObject = {}
+
+export interface IClassInterface<D> {
+  store: Store<any>;
+  models: Array<IModel<any, any>>;
+  dispatchers: D ; 
+  require: (model: any) => void;
+  applyLogicMiddleware: (middleware: any, deps: any) => void;
+  init: (reducers: any, deps: any) => void;
+  getDispatcher: () => D;
+}
+
+export default class Eduxora<D> implements IClassInterface<D> {
+  public dispatchers!: any;
+  public store!: Store<any>;
+  public models: Array<IModel<any, any>>;
+  // public dispatchers!: D;
+  private hasStore: boolean;
+  private logics: Array<any>;
+
+  constructor() {
+    // this.store = {};
+    this.hasStore = false;
     this.models = [];
-    this.dispatchers = initialDispatchers;
+    this.dispatchers = {}
+    // this.dispatchers['asdf'] = 'asdf'
+    // this.dispatchers = dispatchers;
+    this.logics = [];
   }
 
-  public init = (middleware?: any) => {
+  public getDispatcher () {
+    return this.dispatchers as D;
+  }
+
+  applyLogicMiddleware(logics:any, deps:any) {
+    let modelLogics: any[] = [];
+    this.models.forEach(model => {
+      if(model.logics && Array.isArray(model.logics)){
+        modelLogics = modelLogics.concat(model.logics)
+      }
+    });
+    this.logics = modelLogics.concat(logics);
+  }
+
+  public init = (reducers?: any, deps?: object, middlewares?: Array<any>) => {
     const reducerObject: IReducers<any> = {};
 
     this.models.forEach(model => {
-      const reducerFunction: reducerType<any> = (state, action) => {
+      const reducerFunction: reducerType<any> = (state = model.initialState, action) => {
         if (Object.keys(model.reducers).includes(action.type)) {
           return model.reducers[action.type](state, action);
         }
-        return model.initialState;
+        return state;
       };
       reducerObject[model.namespace] = reducerFunction;
     });
 
-    this.store = createStore(combineReducers(reducerObject), middleware);
-  };
+    const logicMiddleware = createLogicMiddleware(this.logics, deps);
+    const middleware = applyMiddleware(
+      logicMiddleware
+    );
 
-  public init2 = (dispatchers: D) => {
-    this.dispatchers = dispatchers;
+    // this.store = createStore(combineReducers({ ...reducerObject, ...reducers }), middleware);
+    this.store = createStore(combineReducers(Object.assign({}, reducerObject, reducers)), middleware)
+    this.hasStore = true;
+    // if(!this.dispatchers){
+    //   this.dispatchers = {}
+    // }
+    this.models.forEach(model => {
+      // this.dispatchers = {
+      //   [model.namespace]: 2
+      // }
+      this.dispatchers[model.namespace] = model.generateDispatchers(this.store.dispatch, deps);
+    });
   };
 
   public getStore = () => {
@@ -70,3 +125,22 @@ export default class Eduxora<D> {
     this.models.push(model);
   };
 }
+
+export type InterfaceA<T,D> = (
+  stateMappers: T,
+  dispatchers: D,
+) => any;
+
+// waiting no typescript to implement feature on generic type spread
+const eduxoConnect = <T,D>(stateMappers: (state: any) => T, dispatchers: D) => {
+  return connect(stateMappers, null, (stateProps: T, dispatchProps : any, ownProps: D) => {
+    return Object.assign({}, stateProps, dispatchers, ownProps)
+    // return {
+    //   ...stateProps,
+    //   ...dispatchers,
+    //   ...ownProps,
+    // };
+  });
+};
+
+export { eduxoConnect, createLogic };
